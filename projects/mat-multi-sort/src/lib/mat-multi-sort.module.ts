6cd719159b85247c20b0e9bfeb3ff52c8aea2a2f
Maximilian Balluff <mballuff@it4ipm.de>
2023-01-08 18:06:38 +0100

    replaced legacy imports

--- projects/mat-multi-sort/src/lib/mat-multi-sort.module.ts
+++ projects/mat-multi-sort/src/lib/mat-multi-sort.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatMultiSort } from './mat-multi-sort.directive';
 import { MatMultiSortHeaderComponent } from './mat-multi-sort-header/mat-multi-sort-header.component';
 import { CommonModule } from '@angular/common';
-import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
-import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
-import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatCommonModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
 import { MatMultiSortTableSettingsComponent } from './mat-multi-sort-table-settings/mat-multi-sort-table-settings.component';
-import {MatLegacyDialogModule as MatDialogModule} from '@angular/material/legacy-dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 
 
 @NgModule({
