586b6795c48958937fef8bdcfce89f75be81ae7a
Ronny <Catscratch@users.noreply.github.com>
2025-02-24 16:22:37 +0100

    fix: Update column names on initialization (#215)

--- src/lib/mat-multi-sort-table-settings/mat-multi-sort-table-settings.component.spec.ts
+++ src/lib/mat-multi-sort-table-settings/mat-multi-sort-table-settings.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { MatMultiSortTableSettingsComponent } from './mat-multi-sort-table-settings.component';
+import { TableData } from 'src/public_api';
 
 describe('MatMultiSortTableComponent', () => {
   let component: MatMultiSortTableSettingsComponent;
@@ -14,10 +15,24 @@ describe('MatMultiSortTableComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MatMultiSortTableSettingsComponent);
     component = fixture.componentInstance;
+    component.tableData = new TableData([{id: 'id1', name: 'name1', isActive: true}], {
+      localStorageKey: 'storage_key'
+    });
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component._tableData).toBeTruthy();
+  });
+
+  it('should update column name on init', () => {
+    expect(component._tableData.columns[0].name).toBe('name1');
+
+    const modifiedTableData = new TableData([{id: 'id1', name: 'name1_new', isActive: true}], {
+      localStorageKey: 'storage_key'
+    });
+
+    expect(modifiedTableData.columns[0].name).toBe('name1_new');
   });
 });
