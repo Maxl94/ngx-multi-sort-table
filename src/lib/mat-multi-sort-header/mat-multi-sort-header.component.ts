b217fcd605219126f7518c9da07406de987fc882
Ronny Fritzsche <Ronny.Fritzsche@planyverse.com>
2025-07-25 09:15:01 +0200

    feat: Refactor components to use DI's `inject()` method and update depend

--- src/lib/mat-multi-sort-header/mat-multi-sort-header.component.ts
+++ src/lib/mat-multi-sort-header/mat-multi-sort-header.component.ts
@@ -2,11 +2,11 @@ import {
   Component,
   Input,
   ChangeDetectorRef,
-  Optional,
-  Inject,
   ViewEncapsulation,
   ChangeDetectionStrategy,
-  ElementRef
+  ElementRef,
+  inject,
+  InjectionToken
 } from '@angular/core';
 import {MatSort, matSortAnimations, MatSortHeader, MatSortHeaderIntl} from '@angular/material/sort';
 import { MatMultiSort } from '../mat-multi-sort.directive';
@@ -37,26 +37,36 @@ interface C2MatSortHeaderColumnDef {
   providers: [{provide: MatSort, useExisting: MatMultiSort}],
 })
 export class MatMultiSortHeaderComponent extends MatSortHeader {
-  start = 'asc' as 'asc' | 'desc';
-  @Input('mat-multi-sort-header') id!: string;
+  _intl: MatSortHeaderIntl;
+  _mySort?: MatMultiSort;
+  _myColumnDef?: C2MatSortHeaderColumnDef;
 
-  constructor(public _intl: MatSortHeaderIntl,
-    changeDetectorRef: ChangeDetectorRef,
-    @Optional() public _sort: MatMultiSort,
-    @Inject('C2_SORT_HEADER_COLUMN_DEF') @Optional() public _columnDef: C2MatSortHeaderColumnDef,
-    _focusMonitor: FocusMonitor,
-    _elementRef: ElementRef<HTMLElement>) {
+  start = 'asc' as 'asc' | 'desc';
+  @Input('mat-multi-sort-header') id: string = '';
+
+  constructor() {
+    const _intl = inject(MatSortHeaderIntl);
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const _sort = inject(MatMultiSort, { optional: true });
+    const _columnDef = inject<C2MatSortHeaderColumnDef>(new InjectionToken('C2_SORT_HEADER_COLUMN_DEF'), { optional: true });
+    const _focusMonitor = inject(FocusMonitor);
+    const _elementRef = inject<ElementRef<HTMLElement>>(ElementRef);
+
     super(_intl, changeDetectorRef, _sort, _columnDef, _focusMonitor, _elementRef);
+
+    this._intl = _intl;
+    if(_sort) this._mySort = _sort;
+    if(_columnDef) this._myColumnDef = _columnDef;
   }
 
   _disableViewStateAnimation = false;
 
   _isSorted() {
-    return this._sort.actives.findIndex(activeId => activeId === this.id) > -1;
+    return this._mySort ? this._mySort.actives.findIndex(activeId => activeId === this.id) > -1 : false;
   }
 
   _sortId() {
-    return this._sort.actives.findIndex(activeId => activeId === this.id) + 1;
+    return this._mySort ? this._mySort.actives.findIndex(activeId => activeId === this.id) + 1 : false;
   }
 
   _renderArrow() {
@@ -64,9 +74,9 @@ export class MatMultiSortHeaderComponent extends MatSortHeader {
   }
 
   getSortDirection(): 'asc' | 'desc' | '' {
-    const i = this._sort.actives.findIndex(activeIds => activeIds === this.id);
-    const direction = this._sort.directions[i];
-    return this._isSorted() ? direction : (this.start || this._sort.start);
+    const i = this._mySort ? this._mySort.actives.findIndex(activeIds => activeIds === this.id) : 0;
+    const direction = this._mySort ? this._mySort.directions[i] : 'asc';
+    return this._isSorted() ? direction : (this.start || (this._mySort && this._mySort.start));
   }
 
   _getArrowViewState() {
